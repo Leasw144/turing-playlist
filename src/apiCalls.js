7e3d03f8c8d5b5c14020d00404e537f7e4257da0
Sean Leas <phoenix_24leas.com>
2020-09-02 12:00:46 -0600

    Create api call for posting song

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,3 +11,24 @@ export function getPlaylist () {
       }
     })
 }
+
+export function postSong(songName, artistName, link) {
+  return fetch(`http://localhost:8080/api/v1/playlist`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      songName: songName,
+      artistName: artistName,
+      link: link
+    })
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw response;
+      }
+    })
+}
